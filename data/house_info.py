18d09901e1e0d6b4620a4c979d45743ee2309087
namtd <namtranhtc826@gmail.com>
2024-03-04 15:36:41 +0700

    namtd commit fix bug and add house info object (not use)

--- data/house_info.py
+++ data/house_info.py
@@ -2,4 +2,12 @@ from pydantic import BaseModel
 
 
 class HouseInfo(BaseModel):
-    pass
+    district: str = None
+    ward: str = None
+    house_type: str = None
+    legal_type: str = None
+    number_floor: float = None
+    number_room: int = None
+    area: float = None
+    width: float = None
+    length: float = None
--- main.py
+++ main.py
@@ -3,12 +3,13 @@ import uvicorn
 from common.constant import host, port, hanoi_house_price_predict_model_path
 from service import house_price_service
 import numpy as np
-
+from data.house_info import HouseInfo
 app = FastAPI()
 
 
-@app.get("/hanoi/house/price")
-async def hanoi_price():
+@app.post("/hanoi/house/price")
+async def hanoi_price(data: HouseInfo):
+    print(data)
     features = np.random.rand(1646, )
     house_price = house_price_service.house_price_service(features=features,
                                                           model_path=hanoi_house_price_predict_model_path)
